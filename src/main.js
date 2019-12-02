58780a23755f04f4e240940f00af8dbc44c00366
Student <example@example.com>
2019-12-02 13:14:32 -0800

    add ability to take user input and determines ROI

--- src/main.js
+++ src/main.js
@@ -1,28 +1,45 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import {averageCostOfLiving, salaryByMajor, lifeStyle, schoolType, paybackRate, federalTaxRate, MyEducation} from './data.js';
+import {MyEducation} from './data.js';
+
+$(document).ready(function(){
+  $("#calculator").submit(function(event){
+    event.preventDefault();
+    let userCity = $("#city option:selected").val();
+    let userMajor = $("#major option:selected").val();
+    let userLifeStyle = $("#lifestyle option:selected").val();
+    let userSchoolType = $("#type option:selected").val();
+    let userFrequency = $("#frequency option:selected").val();
+    let userCashToday = $("#cashToday").val();
+    // let myEducation = new MyEducation("English","in state public", "San Francisco","medium", 100000, "fast");
+    let myEducation = new MyEducation(userMajor, userSchoolType, userCity, userLifeStyle, userCashToday, userFrequency);
+
+    // let roundMyEducation = Math.round(myEducation.paybackPeriod);
+    $(".results").text(myEducation.paybackPeriod);
+
+  });
+
+});
 
 
-let myEducation = new MyEducation("English","in state public", "San Francisco","medium", 100000, "fast");
 
 
-
-console.log(myEducation);
-
-// console.log(myEducation.startingSalary());
-
-console.log(averageCostOfLiving[5].living_cost);
-
-console.log(salaryByMajor[4].mid_career_sal + salaryByMajor[0].mid_career_sal);
-
-console.log(lifeStyle[1].cost);
-
-console.log(federalTaxRate);
-
-
-console.log(schoolType[1]);
-
-
-console.log(paybackRate[1].frequency);
+// console.log(myEducation);
+//
+// // console.log(myEducation.startingSalary());
+//
+// console.log(averageCostOfLiving[5].living_cost);
+//
+// console.log(salaryByMajor[4].mid_career_sal + salaryByMajor[0].mid_career_sal);
+//
+// console.log(lifeStyle[1].cost);
+//
+// console.log(federalTaxRate);
+//
+//
+// console.log(schoolType[1]);
+//
+//
+// console.log(paybackRate[1].frequency);
