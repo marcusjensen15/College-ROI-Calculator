1f36f153f3afdfd2846dd8ba5f85cf8aba28f430
Student <example@example.com>
2019-12-03 10:31:57 -0800

    methods written for lease payment beginning testing now

--- src/backend-car-lease.js
+++ src/backend-car-lease.js
@@ -31,6 +31,54 @@ export class MyCarLease{
 
   }
 
+  netCapCost(){
+    let netCap = this.car_MSRP - (this.trade_in_credit + this.down_payment);
+    return netCap;
+  }
 
+  residual_value(){
+    let carResidualValue = this.car_MSRP * 0.55;
+    return carResidualValue;
+  }
+
+  depreciationFeeMonthly(){
+      let depreciationFee = (this.netCapCost - this.residual_value) / this.leaseTerm;
+      return depreciationFee;
+  }
+
+  moneyFactor(){
+    let moneyFactor = this.apr / 2400;
+    return moneyFactor;
+  }
+
+  financingFeeMonthly(){
+    let financingFee = (this.netCapCost + this.residual_value) * this.moneyFactor;
+    return financingFee;
+  }
+
+  salesTaxMonthly(){
+    let salesTax = (this.depreciationFeeMonthly + this.financingFeeMonthly) * this.sales_tax;
+    return salesTax;
+  }
+
+  total_monthly_lease_payment(){
+    let totalMonthlyLease = this.depreciationFeeMonthly + this.financingFeeMonthly + this.salesTaxMonthly;
+    return totalMonthlyLease;
+  }
+
+  depreciationThreeYear(){
+    let threeYearDepreciation = this.depreciationFeeMonthly * this.leaseTerm;
+    return threeYearDepreciation;
+  }
+
+  financingFeeThreeYear(){
+    let threeYearFinancingFee = this.financingFeeMonthly * this.leaseTerm;
+    return threeYearFinancingFee;
+  }
+
+  salesTaxThreeYear(){
+    let threeYearTax = this.salesTaxMonthly * this.leaseTerm;
+    return threeYearTax;
+  }
 
 }
--- src/main.js
+++ src/main.js
@@ -4,41 +4,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import {MyEducation} from './data.js';
 
-$(document).ready(function(){
-  $("#calculator").submit(function(event){
-    event.preventDefault();
-    let userCity = $("#city option:selected").val();
-    let userMajor = $("#major option:selected").val();
-    let userLifeStyle = $("#lifestyle option:selected").val();
-    let userSchoolType = $("input:radio[name=typeOfSchool]:checked").val();
-    let userFrequency = $("input:radio[name=payPercentage]:checked").val();
-    let userCashToday = $("#cashToday").val();
-    // let myEducation = new MyEducation("English","in state public", "San Francisco","medium", 100000, "fast");
-    let myEducation = new MyEducation(userMajor, userSchoolType, userCity, userLifeStyle, userCashToday, userFrequency);
+// $(document).ready(function(){
+//   $("#calculator").submit(function(event){
+//     event.preventDefault();
+//     let userCity = $("#city option:selected").val();
+//     let userMajor = $("#major option:selected").val();
+//     let userLifeStyle = $("#lifestyle option:selected").val();
+//     let userSchoolType = $("input:radio[name=typeOfSchool]:checked").val();
+//     let userFrequency = $("input:radio[name=payPercentage]:checked").val();
+//     let userCashToday = $("#cashToday").val();
+//     // let myEducation = new MyEducation("English","in state public", "San Francisco","medium", 100000, "fast");
+//     let myEducation = new MyEducation(userMajor, userSchoolType, userCity, userLifeStyle, userCashToday, userFrequency);
+//
+//     $(".results").text(myEducation.paybackPeriod);
+//
+//   });
+//
+// });
 
-    $(".results").text(myEducation.paybackPeriod);
-
-  });
-
-});
-
-
-
-
-// console.log(myEducation);
-//
-// // console.log(myEducation.startingSalary());
-//
-// console.log(averageCostOfLiving[5].living_cost);
-//
-// console.log(salaryByMajor[4].mid_career_sal + salaryByMajor[0].mid_career_sal);
-//
-// console.log(lifeStyle[1].cost);
-//
-// console.log(federalTaxRate);
-//
-//
-// console.log(schoolType[1]);
-//
-//
-// console.log(paybackRate[1].frequency);
+//above this line all works test car lease below
